b6bfcea2f7d0b9fb126d24fa68641438454e9bb5
Sagar Singh Bhola <78876290+sagbho@users.noreply.github.com>
2023-10-23 20:15:07 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const trimenu = document.querySelector('.trimenu');
-const mobileMenu = document.querySelector('.mobile-menu')
-
-trimenu.addEventListener('click', function () {
-    this.classList.toggle('is-active');
-    mobileMenu.classList.toggle('is-open');
-});
-
